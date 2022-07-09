aa2693093719338d7a1fc1d7af1b2eb8ace41444
babelhouse <94088070+babelhouse@users.noreply.github.com>
2022-07-09 08:25:54 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-let now = new Date();
-
-let h3 = document.querySelector("h3");
-let hours = now.getHours();
-if (hours < 10){
-  hours = `0${hours}`;
-
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes <10){
- minutes = `0${minutes}`;
+
+function displayTempreture(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[now.getDay()];
-
-h3.innerHTML = `${day} ${hours}:${minutes} `;
-
-
-
-function showWeather(response){
-document.querySelector("#city").innerHTML=response.data.name;
-document.querySelector("#temperature").innerHTML=Math.round(response.data.main.temp);
-document.querySelector("#humidity").innerHTML=(response.data.main.humidity)
-document.querySelector("#wind").innerHTML=(response.data.wind.speed)
-
-
-}
-function search(event){
+function search(event) {
   event.preventDefault();
-  let apiKey ="5aac6d0188c6f17d6d2bbe6591b6fef0";
+
+  let apiKey = "bd1307acae2205d5435c11972f7bde51";
   let city = document.querySelector("#search-text").value;
-  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl)
-  axios.get(apiUrl).then(showWeather)
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTempreture);
 }
 
 let form = document.querySelector("#search-form");
-
-form.addEventListener("submit", search);
