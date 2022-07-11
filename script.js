3e9dfb31914a0d8bbae10dd8376393ab66f9fe87
babelhouse <94088070+babelhouse@users.noreply.github.com>
2022-07-11 18:38:54 -0400

    updated forecast api dummy data with real data

--- script.js
+++ script.js
@@ -23,29 +23,27 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response){
-  console.log(response.data.daily)
-  let forecastElement=document.querySelector ("#forecast");
-  
-  let days =["Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
  
+  let forecast = response.data.daily;
+  let forecastElement=document.querySelector ("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay){
 
   
   forecastHTML = forecastHTML +`
  
   <div class="col-2">
     <div class="weather-forecast-date">
-    ${day}
+    ${forecastDay.dt}
   </div>
-    <img src="https://openweathermap.org/img/wn/01d@2x.png" 
+    <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
     alt="" 
     width="42"
     />
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max">18°</span> 
-      <span class="weather-forecast-temperature-min">12°</span>
+      <span class="weather-forecast-temperature-max">${forecastDay.temp.max}°</span> 
+      <span class="weather-forecast-temperature-min">${forecastDay.temp.min}°</span>
     </div>
     
   </div>
